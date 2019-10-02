3563c5bcc8ef8c60aae54f76f639a35e7ec36ff6
Alexandre Bardot <bardotalexandre@gmail.com>
2019-10-02 17:00:17 +0200

    ajout 1.0 de la faq

---
+++ faq.js
@@ -0,0 +1,12 @@
+      function showText(num){
+            
+             let reponse1 = document.getElementById('reponse'+num);
+
+             if (reponse1.style.display == "none"){
+              reponse1.style.display = "block";
+             }
+             else {
+              reponse1.style.display = "none";
+             }
+
+           }
