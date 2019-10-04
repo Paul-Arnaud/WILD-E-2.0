96761c4250cc0bfd4c420e0bfe0b79469c6689e8
Paul-Arnaud Lacroix <paularnaudlacroix@gmail.com>
2019-10-04 10:43:59 +0200

    added modals to show images in full size when you click them

--- script.js
+++ script.js
@@ -97,6 +97,23 @@ anime.timeline({loop: true})
     })
   })
 
+  function modal(y) {
+    // Get the modal
+    var modal = document.getElementById('modal'+y);
+    // Get the button that opens the modal
+    var image = document.getElementById("image"+y);
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementById("close"+y);
+    
+    // When the user clicks on the button, open the modal
+        modal.style.display = "block";
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+  }
+
       // fonction pour afficher / masquer une side nav (on ne s'en sert plus)
       // function shownav() {
       //   var plus = document.getElementById("btn-plus");
